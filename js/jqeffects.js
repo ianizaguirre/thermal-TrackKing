9a1a248c07bbcdffec782a6a175c041dba9500e4
[ianizaguirre] <ian@izaguir.re>
2017-11-12 05:42:40 -0500

    added loader

--- js/jqeffects.js
+++ js/jqeffects.js
@@ -1,8 +1,11 @@
 "use strict";
+
+// $( ".row3" ).removeClass('hide-this')();
 $( ".row3" ).slideToggle();
 
 // Event Handler
 $('.monitor-btn').click( function () {
+
 
 
 $( ".row3" ).slideToggle(1000);
