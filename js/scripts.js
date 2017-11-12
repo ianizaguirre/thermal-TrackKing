d368a5d562f48c0567cfc847e310260ec7314ace
Ariel Fonticiella <arielfonticiella@Ariels-MacBook-Pro.local>
2017-11-12 05:42:00 -0500

    added legend to chart

--- js/scripts.js
+++ js/scripts.js
@@ -15,8 +15,7 @@ var lineChartData = {
         strokeColor: "rgba(151,187,205,1)",
         pointColor: "rgba(151,187,205,1)",
         data: [60, 10, 40, 30, 80, 30, 20]
-    }]
-
+    }],
 };
 
 Chart.defaults.global.animationSteps = 50;
