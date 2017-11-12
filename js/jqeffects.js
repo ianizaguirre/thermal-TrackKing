e0381e5a9ffae821fa3e95586e6a08791b053e66
Ariel Fonticiella <arielfonticiella@Ariels-MacBook-Pro.local>
2017-11-12 06:36:49 -0500

    fixed some errors with spacing

--- js/jqeffects.js
+++ js/jqeffects.js
@@ -1,20 +1,12 @@
-<<<<<<< HEAD
 "use strict";
 
 // $( ".row3" ).removeClass('hide-this')();
-=======
 
->>>>>>> d368a5d562f48c0567cfc847e310260ec7314ace
 $( ".row3" ).slideToggle();
 
 // Event Handler
 $('.monitor-btn').click( function () {
 
-<<<<<<< HEAD
-
-
-=======
->>>>>>> d368a5d562f48c0567cfc847e310260ec7314ace
 $( ".row3" ).slideToggle(1000);
 
 }); // event END
