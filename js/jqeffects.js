900c42984313382b47c30bfe013326ef60c371d9
Ariel Fonticiella <arielfonticiella@Ariels-MacBook-Pro.local>
2017-11-12 04:25:00 -0500

    added glow feature to monitor buttons

--- js/jqeffects.js
+++ js/jqeffects.js
@@ -1,12 +1,16 @@
-"use strict";
+
 $( ".row3" ).slideToggle();
 
 // Event Handler
 $('.monitor-btn').click( function () {
 
+$( ".row3" ).slideToggle(1000);
+
+}); // event END
+
+// Event Handler
+$('.monitor-btn-red').click( function () {
 
 $( ".row3" ).slideToggle(1000);
 
-
-
-}); // event END
+});// event END
